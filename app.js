8d53d3a086af3d96aa15aa46e0f15fcf7bfa44bc
David Golden <davidgoldeninbox@gmail.com>
2017-12-21 00:49:35 +0000

    stupid mistake

--- app.js
+++ app.js
@@ -60,6 +60,6 @@ app.get('*', function(req, res) {
     res.redirect('/');
 })
 
-app.listen(process.env.PORT, process.env.IP, function() {
-    console.log('Server has started!!');
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
